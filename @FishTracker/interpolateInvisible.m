dbb021b8da8267d420030c13a9ddc47a29ed4737
malte <malte@e66550d1-145b-42a1-9778-e81285e233c0>
2016-03-04 09:27:13 +0000

    interpolation of the invisible frames
    git-svn-id: svn+ssh://z820/home/svnroot/FishTracker@122 e66550d1-145b-42a1-9778-e81285e233c0

---
+++ @FishTracker/interpolateInvisible.m
@@ -0,0 +1,45 @@
+function out = interpolateInvisible(self,field,kwidth)
+% X = INTERPOLATEINVISIBLE(SELF,FIELD) returns the requested field
+% in res.tracks with interpolated the consecutive invisible
+% frames. INTERPOLATEINVISIBLE(..,kwidth) smothes the data along
+% the frame dimension with running average  kernel.
+  
+  if isempty(self.res)
+    return;
+  end
+  msk = self.getInvisibleMsk();
+  
+  if strcmp(field,'pos')
+    out = self.res.pos;
+    out(permute(cat(3,msk,msk),[1,3,2])) = NaN;
+  elseif isfield(self.res.tracks,field)
+    % always nFrames x nFish x nOther
+    out = double(self.res.tracks.(field));
+    sz = size(out);
+    out = reshape(out,numel(msk),[]);
+    out(msk,:) = NaN;
+    out = reshape(out,sz);
+  else
+    error(sprintf('Field %s does not exist in res.tracks.',field));
+  end
+
+  sz = size(out);
+  for i = 1:prod(sz(2:end))
+    x = out(:,i);
+    msk1 = ~isnan(x);
+    msk2 = ~msk1;
+    if ~all(msk1)
+      ix = interp1(find(msk1),x(msk1),find(msk2),'linear','extrap');
+      x(msk2) = ix;
+    end
+    out(:,i) = x;
+  end
+  
+
+  %%%% GAUSSIAN KERNEL !
+  if nargin>2 && kwidth>0
+    sz = size(out);
+    out = convn(x,ones(nconv,1)/nconv,'same');
+  end
+  
+end

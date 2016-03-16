777d598a5e0d74ad230d22a0fdaa98b2c7c7425a
malte <malte@e66550d1-145b-42a1-9778-e81285e233c0>
2016-03-16 11:47:51 +0000

    resolved the private folder bug in matlab
    git-svn-id: svn+ssh://z820/home/svnroot/FishTracker@130 e66550d1-145b-42a1-9778-e81285e233c0

--- +fish/@Tracker/plotDomains.m
+++ +fish/@Tracker/plotDomains.m
@@ -1,4 +1,4 @@
 function plotDomains(self,varargin)
 % PLOTDOMAINS(PLOTTIMERANGE,FISHIDS) plots a domain overview. 
-  self.plotByType('DOMAINS',varargin{:});
+  plotByType(self,'DOMAINS',varargin{:});
 end

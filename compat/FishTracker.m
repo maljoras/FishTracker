522fffef1a26cfaaed705b0257f02838b4209e8c
Maljoras <malte.rasch@bnu.edu.cn>
2016-03-30 18:31:19 +0800

    developmental branch init

---
+++ compat/FishTracker.m
@@ -0,0 +1,15 @@
+classdef FishTracker
+% dummy for loading the old objects
+   
+  
+  
+  methods(Static)
+    
+    function obj = loadobj(S);
+      obj = fish.Tracker.loadobj(S);
+    end
+  end
+end
+
+    
+
---
+++ compat/FishVideoHandlerMex.m
@@ -0,0 +1,17 @@
+classdef FishVideoHandlerMex
+% dummy for loading the old objects
+   
+  
+  
+  methods(Static)
+    
+    function obj = loadobj(S);
+      obj = FishVideoHandlerMex();
+    end
+  end
+
+
+end
+
+    
+

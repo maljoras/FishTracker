8ec5e4b39ef3bd3fcdd61cf36f27fede6fc9d1a4
maljoras <malte.rasch@bnu.edu.cn>
2016-06-14 15:43:01 +0800

    online learning bugfixes

--- exps/onlinelearning.m
+++ exps/onlinelearning.m
@@ -6,7 +6,7 @@ if LOAD && ~exist('ft','var')
   videoFile = ''; %'/data/videos/onlinelearning/test.avi';
   opts = [];
   opts.detector.inverted = 1;
-  opts.nfish = 2;
+  opts.nfish = 4;
   opts.stmif = 1;
   opts.stimulus.screen = 1;
   %opts.stimulus.screenBoundingBox = sbbox;
@@ -23,7 +23,7 @@ if COMPUTE
 
   if ~exist('sbbox','var')
     %sbbox = ft.calibrateStimulusScreen();
-    sbbox = [0.1589    0.0601    1.5647    1.2685]*1e3;
+    sbbox = [81,83,1570,1231];
   end
   ft.setOpts('stimulus.screenBoundingBox',sbbox,'display.displayEveryNFrame',100,'stimulus.stmSize',100,'stimulus.stmBkgType','texture','stimulus.stmLambda',0,'stimulus.stmTime',15,'stimulus.gapTime',1,'stimulus.midline',0.5,'stimulus.usePredFishId',false);
   
